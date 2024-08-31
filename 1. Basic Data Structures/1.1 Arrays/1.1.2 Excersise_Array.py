ccdda7a711fcd675d53e5920b79088831fa84afd
Fherlg <falg.0711@gmail.com>
2024-08-31 14:39:01 -0600

    lettcodes

--- 1. Basic Data Structures/1.1 Arrays/1.1.2 Excersise_Array.py
+++ 1. Basic Data Structures/1.1 Arrays/1.1.2 Excersise_Array.py
@@ -11,7 +11,7 @@
 #   4. June month just finished and your expense is 1980 dollar. Add this item to our monthly expense list
 #   5. You returned an item that you bought in a month of April and got a refund of 200$. Make a correction to your monthly expense list based on this
 
-monthExpense = {
+monthExpense = { #in this case i used a dictionary
     "January": 2200,
     "February": 2350,
     "March": 2600,
