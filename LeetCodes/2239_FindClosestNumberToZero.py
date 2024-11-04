093a7a3a157fcab689f45837abd8b14f4f7dfefc
Fher García <fernanAnt.0711@hotmail.com>
2024-11-04 13:31:47 -0600

    2239_complexity_change

--- LeetCodes/2239_FindClosestNumberToZero.py
+++ LeetCodes/2239_FindClosestNumberToZero.py
@@ -2,17 +2,26 @@
 # If there are multiple answers, return the number with the largest value.
 
 def findClosestNumber(nums: list[int]) -> int:
-    min = nums[0]
-    for i in range(len(nums)):
-        if abs(nums[i]) < abs(min):
-            min = nums[i]
-    if abs(min) in range(len(nums)): #compare if min positive is in the list
-        return abs(min)
-    return min
+    #this sol have complex O(n)
+    # min = nums[0]
+    # for i in range(len(nums)):
+    #     if abs(nums[i]) < abs(min):
+    #         min = nums[i]
+    # if abs(min) in range(len(nums)): #compare if min positive is in the list
+    #     return abs(min)
+    # return min
+
+    #this one have O(n log n) complexity
+    nums.sort()
+    res = float('inf')
+    for num in nums:
+        if abs(num) < abs(res) or (abs(num) == abs(res) and num > res):
+            res = num
+    return res
 
 
 ex = [-4,-2,1,4,8]
 #ex2 = [2, -1, 1]
 ex3 = [-10000, -10000]
-ex2 = [int(x) for x in input().split()]
-print(findClosestNumber(ex2))
+#ex2 = [int(x) for x in input().split()]
+print(findClosestNumber(ex))
