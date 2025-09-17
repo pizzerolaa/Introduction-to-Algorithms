50d9dc256e3446bf0abf611e9972393fa671bd8e
pizzerolaa <A01643685@tec.mx>
2025-09-17 13:58:08 -0600

    242. Valid Anagram (againn nfw)

--- LeetCodes/242. Valid_Anagram.py
+++ LeetCodes/242. Valid_Anagram.py
@@ -25,3 +25,18 @@ print(is_anagram(s, t))
 #Simple solution and most efficent:
 # def is_anagram(s: str, t: str) -> bool:
 #     return Counter(s) == Counter(t) 
+
+#my solution this can be used on interviews
+# def isAnagram(s: str, t: str) -> bool:
+#     if len(s) < len(t): # if s is smallest than t, return False
+#         return False
+#     #re-use code of leetcode 383 lol
+#     aux = {}
+#     for char in t:
+#         aux[char] = aux.get(char, 0) + 1
+#
+#     for char in s:
+#         if char not in aux or aux[char] == 0:
+#             return False
+#         aux[char] -= 1
+#     return True
