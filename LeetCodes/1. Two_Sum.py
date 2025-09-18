e3ca9193809dddf8fb957402ea07ee6a72cb1343
pizzerolaa <A01643685@tec.mx>
2025-09-18 14:02:29 -0600

    1. Two Sum (this is the n time i do this lol)

--- LeetCodes/1. Two_Sum.py
+++ LeetCodes/1. Two_Sum.py
@@ -5,23 +5,16 @@
 # Example 1:
     # Input: nums = [2,7,11,15], target = 9
     # Output: [0,1]
-# Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+# auxlanation: Because nums[0] + nums[1] == 9, we return [0, 1].
 
 def two_sum(nums: list[int], target: int) -> list[int]:
-    new_nums = [(nums[i], i) for i in range(len(nums))]
-    new_nums.sort()
-    
-    l, r = 0, len(new_nums) - 1
-    
-    while l < r:
-        suma = new_nums[l][0] + new_nums[r][0]
-        if suma == target:
-            return ([new_nums[l][1], new_nums[r][1]])
-        elif suma < target:
-            l += 1
-        else:
-            r -= 1
-    
+    #this solution have a complex O(n) more efficiently, here we use a dict for save the nums and their index
+    aux = {}
+    for index, num in enumerate(nums):
+        diff = target - num
+        if diff in aux:
+            return [aux[diff], index]
+        aux[num] = index
     return []
 
 nums = [3,2,4]
@@ -29,16 +22,22 @@ target = 6
 input()
 print(two_sum(nums=nums, target=target))
 
-#this solution have a complex O(n) more efficiently, here we use a dict for save the nums and their index
+#my other solution bro, using two pointers lol:
 # def twoSum(nums: list[int], target: int) -> list[int]:
-#     aux = {}
-#     for i, num in enumerate(nums):
-#         comp = target - num
-#         if comp in aux:
-#             return [aux[comp], i]
-#         aux[num] = i
-#     return [] #if we don't found a couple 
-    
+#   new_nums = [(nums[i], i) for i in range(len(nums))]
+#   new_nums.sort()
+
+#   l, r = 0, len(new_nums) - 1
+
+#   while l < r:
+#       suma = new_nums[l][0] + new_nums[r][0]
+#       if suma == target:
+#           return ([new_nums[l][1], new_nums[r][1]])
+#       elif suma < target:
+#           l += 1
+#       else:
+#           r -= 1
+
 # This solution have a complex O(n²) in the worst case, but obviusly is more faster that others solutions
 #def twoSum(nums: list[int], target: int) -> list[int]:
 # aux = set(nums)
